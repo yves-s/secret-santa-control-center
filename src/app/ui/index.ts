cf6da8804d9f70111cac75deb4702c92835e26a2
Yves Schleich <yves.schleich@gmail.com>
2016-09-11 21:02:50 +0200

    Renamed secret-santa-input to secret-santa-creator

--- src/app/ui/index.ts
+++ src/app/ui/index.ts
@@ -2,4 +2,4 @@
  * Created by yves.schleich on 29/08/16.
  */
 export {SecretSanta} from './secret-santa';
-export {SecretSantaInput} from './secret-santa-input';
+export {SecretSantaCreator} from './secret-santa-creator';
---
+++ src/app/ui/secret-santa-creator/secret-santa-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'secret-santa-creator',
+  templateUrl: 'secret-santa-creator.component.html',
+  styleUrls: ['secret-santa-creator.component.scss']
+})
+export class SecretSantaCreator implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
