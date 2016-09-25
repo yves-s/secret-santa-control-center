e0740051cef90a06b2aa1492eaa85a2a291905dd
Yves Schleich <yves.schleich@gmail.com>
2016-09-25 23:00:27 +0200

    Use store during lifecycle

--- src/app/containers/control-center/control-center.component.ts
+++ src/app/containers/control-center/control-center.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SecretSanta} from "../../interfaces";
 import {SecretSantaService} from "../../services";
+import {Store} from "../../store";
 
 @Component({
   selector: 'control-center',
@@ -11,11 +12,16 @@ export class ControlCenter implements OnInit {
   secretSantas: SecretSanta[] = [];
 
   constructor(
-    private secretSantaService: SecretSantaService
+    private secretSantaService: SecretSantaService,
+    private store: Store
   ) { }
 
   ngOnInit() {
     this.secretSantas = this.secretSantaService.getSecretSantas();
+    this.store.changes.pluck('secretSantas').
+      subscribe((secretSantas: any) => {
+        console.log('ngOnInit', secretSantas);
+      });
   }
 
   onCreateSecretSanta(secretSanta: SecretSanta) {
